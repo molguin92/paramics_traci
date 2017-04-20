8bcc3d0972395fd70ac344095994ec7066388943
Manuel Olguin <molguin@dcc.uchile.cl>
2017-04-20 20:44:59 -0300

    Network: class -> namespace

--- src/TraCIAPI/Network.h
+++ src/TraCIAPI/Network.h
@@ -5,20 +5,17 @@
 
 namespace traci_api
 {
-    class Network
+    namespace  Network
     {
-    private:
-        static const uint8_t VAR_LNK_LST = 0x00;
-        static const uint8_t VAR_LNK_CNT = 0x01;
+         const uint8_t VAR_LNK_LST = 0x00;
+         const uint8_t VAR_LNK_CNT = 0x01;
 
-        static const uint8_t VAR_NDE_LST = 0x00;
-        static const uint8_t VAR_NDE_CNT = 0x01;
-        static const uint8_t VAR_NDE_POS = 0x42;
-        static const uint8_t VAR_NDE_SHP = 0x4e;
+         const uint8_t VAR_NDE_LST = 0x00;
+         const uint8_t VAR_NDE_CNT = 0x01;
+         const uint8_t VAR_NDE_POS = 0x42;
+         const uint8_t VAR_NDE_SHP = 0x4e;
 
-    public:
-
-        static void getLinkVariable(tcpip::Storage& input, tcpip::Storage& output) throw(traci_api::NoSuchLNKError);
-        static void getJunctionVariable(tcpip::Storage& input, tcpip::Storage& output) throw(traci_api::NoSuchNDEError);
+         void getLinkVariable(tcpip::Storage& input, tcpip::Storage& output) throw(traci_api::NoSuchLNKError);
+         void getJunctionVariable(tcpip::Storage& input, tcpip::Storage& output) throw(traci_api::NoSuchNDEError);
     };
 }
