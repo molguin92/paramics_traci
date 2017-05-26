1a3ca466b0b86c223c2dc0621b0f779184a02053
Manuel Olguin <molguin@dcc.uchile.cl>
2017-05-26 13:16:27 -0400

    new data, information on sent and received warnings and if vehicle reached destination or not

--- experiment_results/analysis.py
+++ experiment_results/analysis.py
@@ -5,17 +5,17 @@ import pandas
 import re
 
 import numpy
-from threading import Thread
 import matplotlib
 from matplotlib import pyplot
 
 matplotlib.style.use('ggplot')
 pyplot.interactive(False)
 
+
 def get_speed_stats(speed_data_path):
     df = pandas.read_csv(speed_data_path, sep=',', thousands=',')
     try:
-        node_nr = re.search('RSUExampleScenario.(.+?).veinsmobility.speed', df.columns[1]).group(1)
+        node_nr = re.search('ProvidenciaExampleScenario.(.+?).veinsmobility.speed', df.columns[1]).group(1)
     except AttributeError:
         node_nr = '??'  # apply your error handling
     df.columns = ['time', 'speed']
@@ -24,27 +24,71 @@ def get_speed_stats(speed_data_path):
 
     return (node_nr, mean, std)
 
-def store_speed_data_csv():
 
-    per10_speedpath = os.path.abspath('.\PER1.0_100Demand\speed_data')
-    per00_speedpath = os.path.abspath('.\PER0.0_100Demand_5secs\speed_data')
+def build_csv():
+    # mobility data
+    mobility_columns = ['module', 'max_speed', 'min_speed', 'start_time', 'stop_time',
+                        'total_co2', 'total_dist', 'total_time']
+    per_00_df_mobility = pandas.read_csv('per0.0_stats_veinsmobility.csv')
+    per_00_df_mobility.columns = mobility_columns
 
-    per10_df = pandas.DataFrame(columns=['node', 'mean_speed', 'std'])
-    per00_df = pandas.DataFrame(columns=['node', 'mean_speed', 'std'])
+    per_10_df_mobility = pandas.read_csv('per1.0_stats_veinsmobility.csv')
+    per_10_df_mobility.columns = mobility_columns
+
+    mobility_search_re = 'ProvidenciaExampleScenario.(.+?).veinsmobility'
+    per_00_df_mobility['module'] = per_00_df_mobility['module'].map(lambda x: re.search(mobility_search_re, x).group(1))
+    per_10_df_mobility['module'] = per_10_df_mobility['module'].map(lambda x: re.search(mobility_search_re, x).group(1))
+
+    per_00_df_mobility.set_index(['module'], inplace=True)
+    per_10_df_mobility.set_index(['module'], inplace=True)
+
+    # appl data (sent warnings, arrived at dest)
+    appl_columns = ['module', 'arrived', 'rcvd_warnings', 'sent_warnings']
+    per_00_df_appl = pandas.read_csv('per0.0_stats_appl.csv')
+    per_00_df_appl.columns = appl_columns
+
+    per_10_df_appl = pandas.read_csv('per1.0_stats_appl.csv')
+    per_10_df_appl.columns = appl_columns
+
+    appl_search_re = 'ProvidenciaExampleScenario.(.+?).appl'
+    per_00_df_appl['module'] = per_00_df_appl['module'].map(lambda x: re.search(appl_search_re, x).group(1))
+    per_10_df_appl['module'] = per_10_df_appl['module'].map(lambda x: re.search(appl_search_re, x).group(1))
+
+    per_00_df_appl['arrived'] = per_00_df_appl['arrived'].map({1: True, 0: False})
+    per_10_df_appl['arrived'] = per_10_df_appl['arrived'].map({1: True, 0: False})
+
+    per_00_df_appl.set_index(['module'], inplace=True)
+    per_10_df_appl.set_index(['module'], inplace=True)
+
+    # build speed data
+    per10_speedpath = os.path.abspath('.\PER1.0_speed_data')
+    per00_speedpath = os.path.abspath('.\PER0.0_speed_data')
+
+    per10_df_speed = pandas.DataFrame(columns=['module', 'mean_speed', 'std_speed'])
+    per00_df_speed = pandas.DataFrame(columns=['module', 'mean_speed', 'std_speed'])
 
     for file in os.listdir(per10_speedpath):
         stats = get_speed_stats(os.path.join(per10_speedpath, file))
-        per10_df.loc[len(per10_df)] = stats
+        per10_df_speed.loc[len(per10_df_speed)] = stats
 
     for file in os.listdir(per00_speedpath):
         stats = get_speed_stats(os.path.join(per00_speedpath, file))
-        per00_df.loc[len(per00_df)] = stats
+        per00_df_speed.loc[len(per00_df_speed)] = stats
 
-    per00_df.set_index(['node'], inplace=True)
-    per10_df.set_index(['node'], inplace=True)
+    per00_df_speed.set_index(['module'], inplace=True)
+    per10_df_speed.set_index(['module'], inplace=True)
 
-    per00_df.to_csv('speed_stats_per00.csv', encoding='utf-8')
-    per10_df.to_csv('speed_stats_per10.csv', encoding='utf-8')
+    # join all tables
+    per00_df = pandas.merge(per_00_df_mobility, per_00_df_appl, left_index=True, right_index=True, how='outer')
+    per10_df = pandas.merge(per_10_df_mobility, per_10_df_appl, left_index=True, right_index=True, how='outer')
+
+    per00_df = pandas.merge(per00_df, per00_df_speed, left_index=True, right_index=True, how='outer')
+    per10_df = pandas.merge(per10_df, per10_df_speed, left_index=True, right_index=True, how='outer')
+
+    per00_df.to_csv('per0.0_total_stats.csv')
+    per10_df.to_csv('per1.0_total_stats.csv')
+
+
 
 def analysis_speed():
     per00_df = pandas.read_csv('speed_stats_per00.csv')
@@ -61,10 +105,6 @@ def analysis_speed():
     pyplot.title('Histograma normalizado de velocidades promedio')
     pyplot.show()
 
-    #fig, axes = pyplot.subplots(nrows=1, ncols=2)
-    #per00_df['mean_speed'].plot.hist(ax=axes[0], color='DarkBlue', bins=10)
-    #per10_df['mean_speed'].plot.hist(ax=axes[1], color='DarkRed', bins=10)
-    #pyplot.show()
 
 def analysis_distance():
     per00_df = pandas.read_csv('./PER0.0_100Demand_5secs_clean.csv', sep=';')
@@ -98,9 +138,8 @@ def analysis_time():
     pyplot.show()
 
 
-
-
 if __name__ == '__main__':
-    analysis_speed()
-    analysis_distance()
-    analysis_time()
+    # analysis_speed()
+    # analysis_distance()
+    # analysis_time()
+    # build_csv()
