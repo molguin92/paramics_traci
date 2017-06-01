bbc605759f8ce5803b40a01c8bc63a4c6de93655
Manuel Olguin <molguin@dcc.uchile.cl>
2017-06-01 14:07:42 -0400

    performance

--- experiment_results/performance_stats/analysis.py
+++ experiment_results/performance_stats/analysis.py
@@ -2,12 +2,21 @@ import matplotlib
 import pandas
 from matplotlib import pyplot
 from psutil import virtual_memory
+import numpy
 
 matplotlib.style.use('ggplot')
 pyplot.interactive(False)
-total_RAM_mb = virtual_memory().total / (1024 * 1024)
 
-def read_data():
+
+def to_min_secs(x, pos):
+    x = int(x)
+    minutes = x // 60
+    seconds = x % 60
+    return '{}:{}'.format(minutes, seconds)
+
+def system_performance():
+    total_RAM_mb = virtual_memory().total / (1024 * 1024)
+
     c = ['timestamp', 'escrituras hdd', 'lecturas hdd',
          'total I/o', '% freq procesador', '% rendimiento procesador',
          '% de uso procesador', '% tiempo procesador', 'MB disponibles RAM']
@@ -37,17 +46,53 @@ def read_data():
     pyplot.xlim([0, 1600])
     pyplot.ylim([0, 60])
 
-    def to_min_secs(x, pos):
-        x = int(x)
-        minutes = x // 60
-        seconds = x % 60
-        return '{}:{}'.format(minutes, seconds)
-
     formatter = matplotlib.ticker.FuncFormatter(to_min_secs)
     ax.xaxis.set_major_formatter(formatter)
 
     pyplot.show()
 
 
+def vehicles_vs_time():
+    cols = ['nvhcs', 't', 'demand', 'runID']
+    df = pandas.read_csv('NVeh_vs_T.csv', encoding='ISO-8859-1', sep=';')
+    df.columns = cols
+
+    df['nvhcs'] = pandas.to_numeric(df['nvhcs'])
+    df['t'] = pandas.to_numeric(df['nvhcs'])
+    df['runID'] = pandas.to_numeric(df['runID'])
+
+    df['demand'] = df['demand'].map(lambda x: float(x.strip('%'))/100)
+
+    print(df)
+
+    fig, ax = pyplot.subplots()
+
+    # trendline
+    z = numpy.polyfit(df['nvhcs'], df['t'], 1)
+    p = numpy.poly1d(z)
+    nx = range(0, int(df['nvhcs'].max()) + 200)
+    ax.plot(nx, p(nx), 'b--', alpha=0.1, label='Ajuste lineal')
+
+    # scatter
+    ax.plot(df.loc[df['demand'] == 1.00]['nvhcs'], df.loc[df['demand'] == 1.00]['t'], 'or', label='Carga 100%')
+    ax.plot(df.loc[df['demand'] == 0.75]['nvhcs'], df.loc[df['demand'] == 0.75]['t'], 'oy', label='Carga 75%')
+    ax.plot(df.loc[df['demand'] == 0.50]['nvhcs'], df.loc[df['demand'] == 0.50]['t'], 'ob', label='Carga 50%')
+    ax.plot(df.loc[df['demand'] == 0.25]['nvhcs'], df.loc[df['demand'] == 0.25]['t'], 'og', label='Carga 25%')
+    ax.legend(loc='upper left')
+    pyplot.ylabel('Tiempo (MM:SS)')
+
+    formatter = matplotlib.ticker.FuncFormatter(to_min_secs)
+    ax.yaxis.set_major_formatter(formatter)
+
+    pyplot.xlabel('Cantidad promedio vehículos en simulación')
+    pyplot.title('Scatterplot: Cantidad promedio de vehículos vs duración en tiempo real de simulación')
+
+    pyplot.xlim([df['nvhcs'].min()-50, df['nvhcs'].max()+50])
+
+
+    pyplot.show()
+
+
 if __name__ == '__main__':
-    read_data()
+    #system_performance()
+    vehicles_vs_time()
